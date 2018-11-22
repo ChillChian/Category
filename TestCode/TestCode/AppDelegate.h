a9f8257962c41e0b6bf3679b823774efce941eaf
qzhq <qzhq1992@qq.com>
2018-11-22 14:21:42 +0800

    初始化

---
+++ TestCode/TestCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCode
+//
+//  Created by Qzhq on 2018/11/22.
+//  Copyright © 2018 Qzhq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCode/TestCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestCode
+//
+//  Created by Qzhq on 2018/11/22.
+//  Copyright © 2018 Qzhq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
